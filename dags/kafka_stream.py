1de14fc4590606170ce13379a55f9b8b6a739a70
SANGAVI PRIYA THYAGARAJAN <e1221539@u.nus.edu>
2024-03-29 10:42:21 +0800

    committing project

---
+++ dags/kafka_stream.py
@@ -0,0 +1,40 @@
+# from datetime import datetime
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+
+# default_args = {
+#     'owner': 'airflow',
+#     'start_date': datetime(2024, 24, 3, 10, 00)
+# }
+
+
+def getdata():
+    import json
+    import requests
+
+    res = requests.get('https://data.cityofchicago.org/resource/85ca-t3if.json')
+    res = res.json()
+    # print(json.dumps(res, indent=3))
+    return res
+
+
+
+def formatdata(res):
+    formatted_data = []
+
+    for record in res:
+        formatted_record= {'crash_record_id': record['crash_record_id'], 'speed_limit': record['posted_speed_limit'],
+                           'weather_condition': record['weather_condition'],
+                           'lightning_condition': record['lighting_condition']}
+        formatted_data.append(formatted_record)
+    return formatted_data
+
+
+def stream_data():
+    import json
+    res = getdata()
+    format_res = formatdata(res)
+    print(json.dumps(format_res, indent=3))
+
+
+stream_data()
