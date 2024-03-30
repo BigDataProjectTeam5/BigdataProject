d8147efbe4bcdab4f41f34dd0fe615d68873d642
Sheila Rezkia <sheilarezkia28@gmail.com>
2024-03-30 17:06:22 +0800

    Create sample DAG

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,11 +1,12 @@
-# from datetime import datetime
-# from airflow import DAG
-# from airflow.operators.python import PythonOperator
+from datetime import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task
 
-# default_args = {
-#     'owner': 'airflow',
-#     'start_date': datetime(2024, 24, 3, 10, 00)
-# }
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 3, 29, 00)
+}
 
 
 def getdata():
@@ -14,7 +15,7 @@ def getdata():
 
     res = requests.get('https://data.cityofchicago.org/resource/85ca-t3if.json')
     res = res.json()
-    # print(json.dumps(res, indent=3))
+    print(json.dumps(res, indent=3))
     return res
 
 
@@ -29,7 +30,7 @@ def formatdata(res):
         formatted_data.append(formatted_record)
     return formatted_data
 
-
+# @task(task_id="fetch_data_from_api")
 def stream_data():
     import json
     res = getdata()
@@ -37,4 +38,13 @@ def stream_data():
     print(json.dumps(format_res, indent=3))
 
 
-stream_data()
+with DAG(
+        'task_chicago_data_populate',
+        schedule='*/1 * * * *',
+        default_args=default_args,
+        catchup=True
+) as dag:
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
