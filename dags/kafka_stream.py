5115b71b43aca81c701ba59397ca391787b47581
SANGAVI PRIYA THYAGARAJAN <e1221539@u.nus.edu>
2024-04-07 18:07:03 +0800

    commit formatted data

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,6 +1,8 @@
 # from datetime import datetime
 # from airflow import DAG
 # from airflow.operators.python import PythonOperator
+from sklearn.ensemble import RandomForestClassifier
+
 
 # default_args = {
 #     'owner': 'airflow',
@@ -8,33 +10,68 @@
 # }
 
 
-def getdata():
+def gettrafficcrashdata():
     import json
     import requests
 
     res = requests.get('https://data.cityofchicago.org/resource/85ca-t3if.json')
     res = res.json()
-    # print(json.dumps(res, indent=3))
+    #print(json.dumps(res, indent=3))
     return res
 
+def gettrafficcongestiondata():
+    import requests
+    import json
+    congestion_raw_data = requests.get('https://data.cityofchicago.org/resource/n4j6-wkkf.json')
+    congestion_raw_data = congestion_raw_data.json()
+    print(json.dumps(congestion_raw_data, indent=3))
+    return congestion_raw_data
 
 
-def formatdata(res):
+
+
+def formattrafficcrashdata(res):
+    import json
     formatted_data = []
 
     for record in res:
-        formatted_record= {'crash_record_id': record['crash_record_id'], 'speed_limit': record['posted_speed_limit'],
-                           'weather_condition': record['weather_condition'],
-                           'lightning_condition': record['lighting_condition']}
+        formatted_record = {
+            "crash_record_id": record.get("crash_record_id"),
+            "crash_date": record.get("crash_date"),
+            "crash_hour": record.get("crash_hour"),
+            "crash_day_of_week": record.get("crash_day_of_week"),
+            "crash_month": record.get("crash_month"),
+            "posted_speed_limit": record.get("posted_speed_limit"),
+            "weather_condition": record.get("weather_condition"),
+            "lighting_condition": record.get("lighting_condition"),
+            "first_crash_type": record.get("first_crash_type"),
+            "trafficway_type": record.get("trafficway_type"),
+            "roadway_surface_cond": record.get("roadway_surface_cond"),
+            "street_alignment": record.get("alignment"),
+            "road_defect": record.get("road_defect"),
+            "crash_type": record.get("crash_type"),
+            "damage": record.get("damage"),
+            "main_cause": record.get("prim_contributory_cause"),
+            "secondary_cause": record.get("sec_contributory_cause"),
+            "street_number": record.get("street_no"),
+            "street_direction": record.get("street_direction"),
+            "street_name": record.get("street_name"),
+            "latitude": record.get("latitude"),
+            "longitude": record.get("longitude"),
+        }
+        print(json.dumps(formatted_record, indent=3))
         formatted_data.append(formatted_record)
     return formatted_data
 
 
 def stream_data():
     import json
-    res = getdata()
-    format_res = formatdata(res)
-    print(json.dumps(format_res, indent=3))
+    res = gettrafficcrashdata()
+    # alert_message = alert_user(res[3])
+    # print(alert_message)
+    format_res = formattrafficcrashdata(res)
+    # print(json.dumps(format_res, indent=3))
+    tc = gettrafficcongestiondata()
 
 
 stream_data()
